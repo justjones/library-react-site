8f966157954d18b31cff1b5b267e20593fe95e34
Lori <firstdiem@gmail.com>
2025-02-07 00:10:41 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './index.css';
+import Nav from './components/Nav'
+
+
+library.add(faBars);
+
+function App() {
+  return (
+    <div className="App">
+     <Nav />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Nav.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fontawesome/react-fontawesome';
+
+const Nav = () => {
+    return (
+        <nav>
+           <div className="nav__container">
+            <a href='/'>
+            <img src='' alt='' className='logo' />
+            </a>
+            <ul className="nav__links">
+                <li className="nav__list">
+                    <a href="/" className='nav__link'>
+                    Home
+                    </a>
+                </li>
+                <li className="nav__list">
+                    <a href="/" className='nav__link'>
+                    Home
+                    </a>
+                </li>
+                <button className='btn__menu'></button>
+                <FontAwesomeIcon icon='bars' />
+            </ul>
+            </div> 
+        </nav>
+    );
+}
+
+export default Nav;
