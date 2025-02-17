0bb10d5ac27a80a73f6c38162b29776b5411a67b
Lori <firstdiem@gmail.com>
2025-02-16 19:44:10 -0600

    final for e-com site

--- src/App.js
+++ src/App.js
@@ -11,14 +11,35 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [cart, setCart] = useState([]);
+
   function addToCart(book) {
-    setCart ([...cart, {...book, quntity: 1}])
+    setCart ([...cart, {...book, quantity: 1}])
   }
 
-  function changeQuantity(book){
-    console.log(book.quntity);
+  function changeQuantity(book, quantity){
+    setCart(cart.map(item => 
+         item.id === book.id
+          ? {
+            ...item,
+            quantity: +quantity,
+          }
+        : item
+      )
+    );
   }
-   
+
+  function removeItem(item){
+    setCart(cart.filter(book => book.id !== item.id))
+  }
+
+  function numberOfItems(){
+    let counter = 0;
+    cart.forEach(item => {
+      counter += item.quantity
+    })
+    return counter;
+  }
+
   useEffect(() => {
     console.log(cart);
   }, [cart])
@@ -26,11 +47,21 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <Nav />
+        <Nav numberOfItems={numberOfItems()}/>
         <Route path="/" exact component={Home} />
         <Route path="/books" exact render={() => <Books books={books} />}/>
-        <Route path='/books/:id' render={() => <BookInfo books={books} addToCart={addToCart} cart={cart}/>} />
-        <Route path='/cart' render={() => <Cart books={books} />} cart={cart} />
+        <Route path='/books/:id' render={() => <BookInfo books={books} addToCart={addToCart} cart={cart} changeQuantity={changeQuantity}/>} />
+        <Route path='/cart'
+         render={() => (
+         <Cart 
+         books={books} 
+         cart={cart} 
+         changeQuantity={changeQuantity} 
+         removeItem={removeItem} 
+         />
+        )} 
+        />
+
         <Footer />        
       </div>
     </Router>
--- src/components/ui/Book.jsx
+++ src/components/ui/Book.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import Rating from "./Rating";
 import Price from "../Price";
@@ -7,21 +7,52 @@ import Price from "../Price";
 
 
 const Book = ({ book }) => {
+    const [img, setImg] = useState(null);
+    const mountedRef = useRef(true); // Fixed typo
+
+    useEffect(() => {
+        mountedRef.current = true; // Ensure ref is set to true when effect runs
+
+        const image = new Image();
+        image.src = book.url;
+        image.onload = () => {
+            setTimeout(() => {
+                if (mountedRef.current) {
+                    setImg(image);
+                }
+            }, 300);
+        };
+
+        return () => {
+            mountedRef.current = false; // Cleanup to prevent memory leaks
+        };
+    }, [book.url]); // Added dependency array to avoid infinite re-renders
 
     return (
         <div className="book">
-            <Link to={`/books/${book.id}`}>
-                <figure className="book__img--wrapper">
-                    <img src={book.url} alt="" className="book__img" />
-                </figure>
-            </Link>
-            <div className="book__title">
-                <Link to={`/books/${book.id}`} className='book__title--link'>
-                    {book.title}
-                </Link>
-            </div>
-            <Rating rating={book.rating} />
-            <Price salePrice={book.salePrice} originalPrice={book.originalPrice} />
+            {img ? (
+                <>
+                    <Link to={`/books/${book.id}`}>
+                        <figure className="book__img--wrapper">
+                            <img src={img.src} alt={book.title} className="book__img" />
+                        </figure>
+                    </Link>
+                    <div className="book__title">
+                        <Link to={`/books/${book.id}`} className="book__title--link">
+                            {book.title}
+                        </Link>
+                    </div>
+                    <Rating rating={book.rating} />
+                    <Price salePrice={book.salePrice} originalPrice={book.originalPrice} />
+                </>
+            ) : (
+                <>
+                    <div className="book__img--skeleton"></div>
+                    <div className="skeleton book__title--skeleton"></div>
+                    <div className="skeleton book__rating--skeleton"></div>
+                    <div className="skeleton book__price--skeleton"></div>
+                </>
+            )}
         </div>
     );
 };
